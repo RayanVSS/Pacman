23b19dffad55264b1953e35420a74fd3eb95b9bd
BENCHEIKH Ilias <ilias.bencheikh@etu.u-paris.fr>
2023-10-12 16:14:03 +0200

    Replace Ghost.java

--- src/main/java/model/Ghost.java
+++ src/main/java/model/Ghost.java
@@ -1,14 +1,19 @@
 package model;
 
+import java.util.List;
+
+import config.MazeConfig;
 import geometry.RealCoordinates;
 
 public enum Ghost implements Critter {
 
-    // TODO: implement a different AI for each ghost, according to the description in Wikipedia's page
+    // TODO: implement a different AI for each ghost, according to the description
+    // in Wikipedia's page
     BLINKY, INKY, PINKY, CLYDE;
 
     private RealCoordinates pos;
     private Direction direction = Direction.NONE;
+
     @Override
     public RealCoordinates getPos() {
         return pos;
@@ -17,6 +22,23 @@ public enum Ghost implements Critter {
     @Override
     public void setPos(RealCoordinates newPos) {
         pos = newPos;
+    }
+
+    @Override
+    public RealCoordinates nextPos(long deltaTNanoSeconds, MazeConfig config) {
+        if (this == INKY) {
+            Direction d = Inky.nexDirectionInky(INKY, PacMan.INSTANCE, config);
+            setDirection(d);
+            setPos(getPos().plus(Inky.DirectionToRealCoordinates(d).times(getSpeed())));
+            return pos;
+        }
+        return getPos().plus((switch (getDirection()) {
+            case NONE -> RealCoordinates.ZERO;
+            case NORTH -> RealCoordinates.NORTH_UNIT;
+            case EAST -> RealCoordinates.EAST_UNIT;
+            case SOUTH -> RealCoordinates.SOUTH_UNIT;
+            case WEST -> RealCoordinates.WEST_UNIT;
+        }).times(getSpeed() * deltaTNanoSeconds * 1E-9));
     }
 
     @Override
@@ -31,7 +53,11 @@ public enum Ghost implements Critter {
 
     @Override
     public double getSpeed() {
+        if (this == INKY) {
+            return 3E-2;
+        }
         return 0;
+
     }
 
 }
