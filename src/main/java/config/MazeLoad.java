909f704690938f0e63c817af3ef203aad36ee757
FERNEZ dimitri <dimitri.fernez@etu.u-paris.fr>
2023-10-14 00:00:09 +0200

    Maze reparation

--- src/main/java/config/MazeLoad.java
+++ src/main/java/config/MazeLoad.java
@@ -17,18 +17,36 @@ public class MazeLoad {
             String content = new String(Files.readAllBytes(Paths.get(file)));
             JSONObject json = new JSONObject(content);
             JSONArray maze = json.getJSONArray("maze");
-            Cell[][] cells = new Cell[maze.length()][maze.getJSONArray(0).length()];
-            for (int i = 0; i < maze.length(); i++) {
-                JSONArray row = maze.getJSONArray(i);
-                for (int j = 0; j < row.length(); j++) {
-                    JSONObject cell = row.getJSONObject(j);
-                    String type = cell.getString("type");
-                    String value = cell.getString("value");
+
+            JSONArray type_list = maze.getJSONArray(0);
+            JSONArray value_list = maze.getJSONArray(1);
+
+            //if the list are not a cube, throw an exception
+            // for (int i = 0; i < type_list.length(); i++) {
+            //     System.out.println(type_list.getJSONArray(i).length());
+            //     System.out.println(value_list.getJSONArray(i).length());
+            //     if (type_list.getJSONArray(i).length() != value_list.getJSONArray(i).length()) {
+            //         throw new IllegalStateException("Les listes ne sont pas des cubes");
+            //     }
+            // }
+
+            // if 2 lists are not the same size, throw an exception
+            if (type_list.length() != value_list.length()) {
+                throw new IllegalStateException("Les listes ne sont pas de la même taille");
+            }
+            Cell[][] cells = new Cell[type_list.length()][type_list.getJSONArray(0).length()];
+
+            for (int i = 0; i < type_list.length(); i++) {
+                JSONArray row_type = type_list.getJSONArray(i);
+                JSONArray row_value = value_list.getJSONArray(i);
+                for (int j = 0; j < row_type.length(); j++) {
+                    String type = row_type.getString(j);
+                    String value = row_value.getString(j);
                     cells[i][j] = Cell.withContent(type, value);
                 }
             }
-            //print(cells);
-            return cells;
+                //print(cells);
+                return cells;
         } catch (IOException e) {
             e.printStackTrace();
         }
