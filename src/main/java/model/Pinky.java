df50ad336007e7e67da97db763f905b93b08049a
BENCHEIKH Ilias <ilias.bencheikh@etu.u-paris.fr>
2023-10-14 17:49:33 +0200

    Replace Pinky.java

--- src/main/java/model/Pinky.java
+++ src/main/java/model/Pinky.java
@@ -5,7 +5,7 @@ import geometry.RealCoordinates;
 
 public class Pinky {
 
-    public static Direction nexDirectionPinky(Ghost PINKY, Critter PacMan, MazeConfig config) {
+    public static Direction nextDirection(Ghost PINKY, Critter PacMan, MazeConfig config) {
         RealCoordinates PacManPos = PacMan.getPos();
         RealCoordinates pinkyPos = PINKY.getPos();
         Direction[] possibleDirections = {
@@ -17,65 +17,25 @@ public class Pinky {
         int i = 0;
         for (Direction dir : possibleDirections) {
             RealCoordinates newPos = pinkyPos
-                    .plus(DirectionToRealCoordinates(dir).times(PINKY.getSpeed()))
-                    .warp(config.getWidth(), config.getHeight());
+                    .plus(outilghost.DirectionToRealCoordinates(dir).times(PINKY.getSpeed()));
 
-            double distance = distance(newPos, PacManPos);
+            double distance = outilghost.distance(newPos, PacManPos);
             tabDistance[i] = distance;
             i += 1;
         }
-        return tri(possibleDirections, tabDistance)[0];
+        return mDirection(outilghost.tri(possibleDirections, tabDistance), config, PINKY);
     }
 
     public static Direction mDirection(Direction[] tab, MazeConfig config, Ghost PINKY) {
         for (Direction dir : tab) {
             RealCoordinates newPos = PINKY.getPos()
-                    .plus(DirectionToRealCoordinates(dir).times(PINKY.getSpeed()))
-                    .warp(config.getWidth(), config.getHeight());
-
-        }
-        return Direction.NONE;
-    }
-
-    public static Direction[] tri(Direction[] directions, double[] distances) {
-        int n = distances.length;
-        Direction[] tab = new Direction[n];
-        for (int i = 1; i < n; i++) {
-            Direction currentDirection = directions[i];
-            double currentDistance = distances[i];
-            int j = i - 1;
-
-            while (j >= 0 && distances[j] > currentDistance) {
-                directions[j + 1] = directions[j];
-                distances[j + 1] = distances[j];
-                j--;
+                    .plus(outilghost.DirectionToRealCoordinates(dir).times(PINKY.getSpeed()));
+            if (newPos.x() == PINKY.getPos().x() || newPos.y() == PINKY.getPos().y()) {
+                return dir;
             }
 
-            directions[j + 1] = currentDirection;
-            distances[j + 1] = currentDistance;
         }
-        return directions;
+        return PINKY.getDirection();
     }
 
-    public static double distance(RealCoordinates point1, RealCoordinates point2) {
-        double deltaX = point1.x() - point2.x();
-        double deltaY = point1.y() - point2.y();
-        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-    }
-
-    // Méthode utilitaire pour convertir une direction en coordonnées
-    public static RealCoordinates DirectionToRealCoordinates(Direction dir) {
-        switch (dir) {
-            case NORTH:
-                return RealCoordinates.NORTH_UNIT;
-            case EAST:
-                return RealCoordinates.EAST_UNIT;
-            case SOUTH:
-                return RealCoordinates.SOUTH_UNIT;
-            case WEST:
-                return RealCoordinates.WEST_UNIT;
-            default:
-                return RealCoordinates.ZERO;
-        }
-    }
 }
