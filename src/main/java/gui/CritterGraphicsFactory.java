87d6786df8adece603432ef59920007cc1514dc7
belhasse <rayan.belhassen@etu.u-paris.fr>
2023-10-16 23:41:38 +0200

    rotation de pacman

--- src/main/java/gui/CritterGraphicsFactory.java
+++ src/main/java/gui/CritterGraphicsFactory.java
@@ -3,6 +3,7 @@ package gui;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
 import model.Critter;
 import model.Ghost;
 import model.PacMan;
@@ -25,12 +26,34 @@ public final class CritterGraphicsFactory {
                     case PINKY -> "ghost_pinky.png";
                 };
         var image = new ImageView(new Image(url, scale * size, scale * size, true, true));
+        Rotate rotation = new Rotate(0, scale * size / 2, scale * size / 2); // Initial rotation angle set to 0
+        image.getTransforms().add(rotation);
+
         return new GraphicsUpdater() {
             @Override
+            
             public void update() {
                 image.setTranslateX((critter.getPos().x() + (1 - size) / 2) * scale);
                 image.setTranslateY((critter.getPos().y() + (1 - size) / 2) * scale);
                 // Debug.out("sprite updated");
+                if (critter instanceof PacMan) {
+                    switch (((PacMan) critter).getDirection()) {
+                        case NORTH:
+                            rotation.setAngle(90);                         
+                            break;
+                        case EAST:
+                            rotation.setAngle(180); 
+                            break;
+                        case WEST:
+                            rotation.setAngle(0); 
+                            break;
+                        case SOUTH:
+                            rotation.setAngle(270); 
+                            break;
+                    }
+                }
+            
+
             }
 
             @Override
