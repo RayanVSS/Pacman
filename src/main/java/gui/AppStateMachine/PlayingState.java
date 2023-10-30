e138489e5cacbae5c2fb62f38632cfb6bafaaa93
CHETOUANI bilal <bilal.chetouani@etu.u-paris.fr>
2023-10-30 14:43:20 +0100

    Résolution d'un bug qui faisait que les éléments n'étaient pas correctement positionnés à CAUSE D'UNE FOUTUE LIGNE JE ZAIUBIUZAufihu

--- src/main/java/gui/AppStateMachine/PlayingState.java
+++ src/main/java/gui/AppStateMachine/PlayingState.java
@@ -39,44 +39,65 @@ public class PlayingState extends App implements State {
         return state_name;
     }
 
-    public void enter() {
-
-        maze = new MazeState(MazeConfig.makeExample1());
-        MazeState.resetScore();
-        maze.setLives(3);
-        score_graphics.setTextAlignment(TextAlignment.LEFT);
-        score_graphics.setFont(App.text_graphics);
-        score_graphics.setTextFill(javafx.scene.paint.Color.WHITE);
-    
+    public Label createLifeGraphics(){
+        Label life_graphics = new Label("" + 3);
+        
         Image img = new Image(getClass().getResourceAsStream("/pixel_heart.png"));
         ImageView view = new ImageView(img);
+        
         view.setPreserveRatio(true);
         view.setFitHeight(50);
         view.setFitWidth(50);
+
         life_graphics.setGraphic(view);
-    
+
         life_graphics.setTextAlignment(TextAlignment.LEFT);
         life_graphics.setFont(App.text_graphics);
         life_graphics.setTextFill(javafx.scene.paint.Color.RED);
+        
+        return life_graphics;
+    }
+
+    public Label createScoreGraphics(){
+        Label score_graphics = new Label();
+        
+        score_graphics.setTextAlignment(TextAlignment.LEFT);
+        score_graphics.setFont(App.text_graphics);
+        score_graphics.setTextFill(javafx.scene.paint.Color.WHITE);
+        
+        return score_graphics;
+    }
+
+    public void enter() {
+        BorderPane root = new BorderPane();
+
+        game_screen.setStyle("-fx-background-color: black;");
+    
+        maze = new MazeState(MazeConfig.makeExample1());
+        MazeState.resetScore();
+        maze.setLives(3);
+    
+        score_graphics = createScoreGraphics();
+        life_graphics = createLifeGraphics();
     
         App.screen.setRoot(game_screen);
     
         var pacmanController = new PacmanController();
         App.screen.setOnKeyPressed(pacmanController::keyPressedHandler);
         App.screen.setOnKeyReleased(pacmanController::keyReleasedHandler);
-        gameView = new GameView(maze, game_screen, 40);
+        gameView = new GameView(maze, root, 30);
+
+        game_screen.setCenter(gameView.getGameRoot());
     
-        score_graphics.toFront(); // Met le score par-dessus le reste des éléments
-        life_graphics.toFront(); // Met l'affichage des vies par-dessus le reste des éléments
-    
-        // Life_graphics dans la partie inférieure du BorderPane
+        // Ajoutez life_graphics en bas du BorderPane
         game_screen.setBottom(life_graphics);
     
-        // Score_graphics dans la partie supérieure du BorderPane
+        // Ajoutez score_graphics en haut du BorderPane
         game_screen.setTop(score_graphics);
     
         gameView.animate();
     }
+    
 
     public void process(long deltaT) {
 
