0253852362b98e83b6da1d9136ac04518c340d60
BENCHEIKH Ilias <ilias.bencheikh@etu.u-paris.fr>
2023-11-01 20:51:03 +0100

    Replace Pinky.java

--- src/main/java/model/Pinky.java
+++ src/main/java/model/Pinky.java
@@ -17,25 +17,13 @@ public class Pinky {
         int i = 0;
         for (Direction dir : possibleDirections) {
             RealCoordinates newPos = pinkyPos
-                    .plus(outilghost.DirectionToRealCoordinates(dir).times(PINKY.getSpeed()));
+                    .plus(outil.DirectionToRealCoordinates(dir).times(PINKY.getSpeed()));
 
-            double distance = outilghost.distance(newPos, PacManPos);
+            double distance = outil.distance(newPos, PacManPos);
             tabDistance[i] = distance;
             i += 1;
         }
-        return mDirection(outilghost.tri(possibleDirections, tabDistance), config, PINKY);
-    }
-
-    public static Direction mDirection(Direction[] tab, MazeConfig config, Ghost PINKY) {
-        for (Direction dir : tab) {
-            RealCoordinates newPos = PINKY.getPos()
-                    .plus(outilghost.DirectionToRealCoordinates(dir).times(PINKY.getSpeed()));
-            if (newPos.x() == PINKY.getPos().x() || newPos.y() == PINKY.getPos().y()) {
-                return dir;
-            }
-
-        }
-        return PINKY.getDirection();
+        return outil.estpossible(outil.tri(possibleDirections, tabDistance), PINKY, config);
     }
 
 }
