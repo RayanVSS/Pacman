3899fc31c9f6b43407c8e139de539a5d583e5679
BENCHEIKH Ilias <ilias.bencheikh@etu.u-paris.fr>
2023-11-02 21:08:54 +0100

    Update Pinky.java

--- src/main/java/model/Pinky.java
+++ src/main/java/model/Pinky.java
@@ -1,29 +1,32 @@
 package model;
 
-import config.MazeConfig;
 import geometry.RealCoordinates;
 
 public class Pinky {
 
-    public static Direction nextDirection(Ghost PINKY, Critter PacMan, MazeConfig config) {
-        RealCoordinates PacManPos = PacMan.getPos();
-        RealCoordinates pinkyPos = PINKY.getPos();
+    public static Direction[] nextDirection(Ghost pinky, Critter pacman) {
+        RealCoordinates pacManPos = pacman.getPos();
+        RealCoordinates pinkyPos = pinky.getPos();
         Direction[] possibleDirections = {
                 Direction.NORTH,
                 Direction.EAST,
                 Direction.SOUTH,
-                Direction.WEST };
+                Direction.WEST
+        };
+        RealCoordinates targetPos = pacManPos
+                .plus(outil.DirectionToRealCoordinates(pacman.getDirection()).times(2 * pacman.getSpeed()));
+
         double[] tabDistance = new double[4];
         int i = 0;
-        for (Direction dir : possibleDirections) {
-            RealCoordinates newPos = pinkyPos
-                    .plus(outil.DirectionToRealCoordinates(dir).times(PINKY.getSpeed()));
 
-            double distance = outil.distance(newPos, PacManPos);
+        for (Direction dir : possibleDirections) {
+            RealCoordinates newPos = pinkyPos.plus(outil.DirectionToRealCoordinates(dir).times(pinky.getSpeed()));
+            double distance = outil.distance(newPos, targetPos);
             tabDistance[i] = distance;
-            i += 1;
+            i++;
         }
-        return outil.estpossible(outil.tri(possibleDirections, tabDistance), PINKY, config);
+
+        return outil.tri(possibleDirections, tabDistance);
     }
 
 }
