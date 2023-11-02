c50e2e990b5a3a4f1162a0f46318704dd3be52d2
BENCHEIKH Ilias <ilias.bencheikh@etu.u-paris.fr>
2023-11-02 21:10:38 +0100

    Update Clyde.java

--- src/main/java/model/Clyde.java
+++ src/main/java/model/Clyde.java
@@ -2,44 +2,43 @@ package model;
 
 import java.util.Random;
 
-import config.MazeConfig;
 import geometry.RealCoordinates;
 
 public class Clyde {
 
-    public static Direction nextDirection(MazeConfig config, Ghost CLYDE, Critter PacMan) {
-        RealCoordinates clydePos = CLYDE.getPos();
-        RealCoordinates pacManPos = PacMan.getPos();
+    public static Direction[] nextDirection(Ghost clyde, Critter pacMan) {
+        RealCoordinates clydePos = clyde.getPos();
+        RealCoordinates pacManPos = pacMan.getPos();
         Direction[] tab = {
                 Direction.NORTH,
                 Direction.EAST,
                 Direction.SOUTH,
-                Direction.WEST };
+                Direction.WEST
+        };
         double distanceToPacMan = outil.distance(clydePos, pacManPos);
+
         if (distanceToPacMan < 5) {
             double[] tabDistance = new double[4];
             int i = 0;
+
             for (Direction dir : tab) {
-                RealCoordinates newPos = clydePos
-                        .plus(outil.DirectionToRealCoordinates(dir).times(CLYDE.getSpeed()));
-
+                RealCoordinates newPos = clydePos.plus(outil.DirectionToRealCoordinates(dir).times(clyde.getSpeed()));
                 double newDistance = outil.distance(newPos, pacManPos);
-
                 tabDistance[i] = newDistance;
+                i++;
             }
-            return outil.estpossible(outil.tri(tab, tabDistance), CLYDE, config);
+
+            return outil.tri(tab, tabDistance);
         } else {
-            return outil.estpossible(trialeatoire(tab), CLYDE, config);
+            return triAleatoire(tab);
         }
     }
 
-    public static Direction[] trialeatoire(Direction[] directions) {
+    public static Direction[] triAleatoire(Direction[] directions) {
         Random rand = new Random();
 
         for (int i = directions.length - 1; i > 0; i--) {
             int index = rand.nextInt(i + 1);
-
-            // Échange les directions aux positions i et index
             Direction temp = directions[i];
             directions[i] = directions[index];
             directions[index] = temp;
