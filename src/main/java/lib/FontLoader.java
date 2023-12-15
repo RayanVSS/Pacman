db63739673926e62c2293f3f106135b0a2fd92f4
Chetouani Bilal <chetouanibilal04@gmail.com>
2023-12-15 15:37:23 +0100

    On peut maintenant réaliser un executable du jeu en lancant lauch4j, pour le jar il faut executer runshadow. Si vous voulez faire du plateform specifique vous pouvez build les binaires à l'aide de jpackage...

--- src/main/java/lib/FontLoader.java
+++ src/main/java/lib/FontLoader.java
@@ -1,30 +1,24 @@
 package lib;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import javafx.scene.text.Font;
 
 public class FontLoader {
     private static Font pixel_font;
     static {
         try {
-            URL fontUrl = FontLoader.class.getResource("/Font/pixel_font.ttf");
-            File f = Paths.get(fontUrl.toURI()).toFile();
-            pixel_font = Font.loadFont(f.toURI().toURL().toString(), 20.0);
+            InputStream fontStream = FontLoader.class.getResourceAsStream("/Font/pixel_font.ttf");
+            pixel_font = Font.loadFont(fontStream, 20.0);
             if (pixel_font == null) {
-                //Pour MacOS jsp pk ça marche pas
+                // Pour MacOS jsp pk ça marche pas
                 pixel_font = Font.loadFont(FontLoader.class.getResourceAsStream("/Font/pixel_font.ttf"), 20);
                 if (pixel_font == null) {
-                    pixel_font = Font.font("Arial", 20); //Default font si ça marche pas
+                    pixel_font = Font.font("Arial", 20); // Default font si ça marche pas
                 }
             }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Erreur lors du chargement de la police de caractère");
         } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("Erreur lors du chargement de la police de caractère");
         }
     }
 
